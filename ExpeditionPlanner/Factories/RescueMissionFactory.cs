d38a2bd3b4f35a2842132e752bc238fd31446e2c
dezue <mistfoll@mail.ru>
2026-04-17 14:47:16 +0700

    fixed

--- ExpeditionPlanner/Factories/RescueMissionFactory.cs
+++ ExpeditionPlanner/Factories/RescueMissionFactory.cs
@@ -3,11 +3,9 @@ using System;
 
 namespace ExpeditionPlanner.Factories {
   public class RescueMissionFactory : MissionFactory {
-    private Random _random = new Random();
 
     public override Mission CreateMission() {
-      int peopleCount = _random.Next(10, 101);
-      return new RescueMission(peopleCount);
+      return new RescueMission(50);
     }
   }
 }
