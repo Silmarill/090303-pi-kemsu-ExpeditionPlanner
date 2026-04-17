2866e1d2274fcb97fb3fce643c86906b3885d6f8
Konstantin <H1dra5@yandex.ru>
2026-04-17 08:52:30 +0700

    Добавил спасательную операцию и работаю на конструктором для фабрики

---
+++ ExpeditionPlanner/Factories/RescueMissionFactory.cs
@@ -0,0 +1,16 @@
+﻿using ExpeditionPlanner.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpeditionPlanner.Factories {
+  public class RescueMissionFactory : MissionFactory {
+    public RescueMissionFactory() { }
+    
+    public override Mission CreateMission() {
+      return new RescueMission();
+    }
+  }
+}
