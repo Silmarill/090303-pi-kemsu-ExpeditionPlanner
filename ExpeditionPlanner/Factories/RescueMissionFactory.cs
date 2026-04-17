3f3277d43445146d368d2b53118b25ceed0c5d7e
komi1488 <pashaandreev66666@gmail.com>
2026-04-17 12:48:30 +0700

    спасательная миссия добавлена

---
+++ ExpeditionPlanner/Factories/RescueMissionFactory.cs
@@ -0,0 +1,19 @@
+﻿using ExpeditionPlanner.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpeditionPlanner.Factories {
+  public class RescueMissionFactory : MissionFactory {
+    private int _peopleCount;
+
+    public RescueMissionFactory(int peopleCount) {
+      _peopleCount = peopleCount;
+    }
+    public override Mission CreateMission() {
+      return new RescueMission(_peopleCount);
+    }
+  }
+}
