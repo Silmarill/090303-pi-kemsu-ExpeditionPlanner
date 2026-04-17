da5042926cf820f65a45a6832df5ff600d765cab
lol <nexusstail@gmail.com>
2026-04-17 09:26:12 +0700

    Добавил RescueMission

---
+++ ExpeditionPlanner/Models/RescueMisson.cs
@@ -0,0 +1,14 @@
+﻿using ExpeditionPlanner.Factories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpeditionPlanner.Models {
+ public class RescueMisson: Mission {
+    private int _peopleCount;
+    public RescueMission  (int peopleCount) :base ("Спасательная миссия",5 + peopleCount /10)
+      _peopleCont=popleCount
+  }
+}
