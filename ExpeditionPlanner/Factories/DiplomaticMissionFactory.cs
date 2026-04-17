7f3e2b64dc0c1ca9b4263fdc3469da2effe803bb
ksmushkov-cmd <ksmushkov@bk.ru>
2026-04-17 15:02:35 +0700

    что-то сделал

---
+++ ExpeditionPlanner/Factories/DiplomaticMissionFactory.cs
@@ -0,0 +1,14 @@
+﻿using ExpeditionPlanner.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpeditionPlanner.Factories {
+  public class DiplomaticMissionFactory : MissionFactory{
+    public override Mission CreateMission() {
+      return new DiplomaticMission();
+    }
+  }
+}
---
+++ ExpeditionPlanner/Models/RescueMission.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpeditionPlanner.Models {
+  public class RescueMission : Mission {
+    private int _peopleCount;
+    public RescueMission(int peopleCount) {
+      _peopleCount = peopleCount;
+      Name = "Спасательная миссия";
+      Duration = 5 + peopleCount / 10;
+    }
+
+    public override void Execute() {
+      Console.WriteLine($"Спасательная операция: эвакуация {_peopleCount} человек");
+    }
+
+    public override string GetReport() {
+      return $"{Name}: спасено {_peopleCount}";
+    }
+  }
+}
