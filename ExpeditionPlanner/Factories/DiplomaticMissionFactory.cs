15de251d9af287eba90bc652dce5bb6b82fad71b
Mateus8637228 <Matrac2k20@gmail.com>
2026-04-17 11:32:56 +0700

    Сделал первый этапчик

---
+++ ExpeditionPlanner/Factories/DiplomaticMissionFactory.cs
@@ -0,0 +1,10 @@
+﻿using ExpeditionPlanner.Models;
+using System;
+
+namespace ExpeditionPlanner.Factories {
+  public class DiplomaticMissionFactory : MissionFactory {
+    public override Mission CreateMission() {
+      return new DiplomaticMission();
+    }
+  }
+}
