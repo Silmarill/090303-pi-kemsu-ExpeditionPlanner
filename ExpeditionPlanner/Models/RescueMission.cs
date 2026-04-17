4f6da9a9d7561fd2c4143d3db66106c41619c5b6
inkoromi21 <sema.vorobev20078@gmail.com>
2026-04-17 08:52:14 +0700

    заставили сделать коммит + пуш на середине работы

---
+++ ExpeditionPlanner/Models/RescueMission.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ExpeditionPlanner.Models {
+  public class RescueMission : Mission {
+    private int _peopleCount;
+    public RescueMission(int peopleCount) {
+      Name = "Спасательская миссия";
+      _peopleCount = peopleCount;
+      Duration = 5 + _peopleCount / 10;
+    }
+
+    public override void Execute() {
+      Console.WriteLine(" Спасательная операция");
+    }
+
+    public override string GetReport() {
+      return $" {Name}: открыто 5 новых звёздных систем";
+    }
+  }
+}
