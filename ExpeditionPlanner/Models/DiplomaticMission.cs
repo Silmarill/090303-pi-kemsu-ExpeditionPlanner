8e5491fd023e31884a1db8d2fdf567592a59c5d3
komi1488 <pashaandreev66666@gmail.com>
2026-04-17 12:29:06 +0700

    DiplomaticMission

---
+++ ExpeditionPlanner/Models/DiplomaticMission.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpeditionPlanner.Models {
+  public class DiplomaticMission : Mission {
+    public DiplomaticMission() {
+      Name = "Дипломатическая миссия";
+      Duration = 30;
+    }
+    public override void Execute() {
+      Console.WriteLine(" Переговоры с инопланетной цивилизацией");
+    }
+
+    public override string GetReport() {
+      return $" {Name}: заключено 3 торговых соглашения";
+    }
+  }
+}
