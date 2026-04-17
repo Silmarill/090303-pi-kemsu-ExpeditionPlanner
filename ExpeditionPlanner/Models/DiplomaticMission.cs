3251a69309d764a07b17174c0e829bd923b7c2d3
Asverktus <aleksandr.rubin123@gmail.com>
2026-04-17 12:37:03 +0700

    Задание 1

---
+++ ExpeditionPlanner/Models/DiplomaticMission.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ExpeditionPlanner.Models {
+  public class DiplomaticMission : Mission {
+    public DiplomaticMission() {
+      Name = "Дипломатическая миссия";
+      Duration = 20;
+    }
+
+    public override void Execute() {
+      Console.WriteLine(" Переговоры с инопланетной цивилизацией, подписание договора");
+    }
+
+    public override string GetReport() {
+      return $" {Name}: заключено 3 торговых соглашения";
+    }
+  }
+
+}
