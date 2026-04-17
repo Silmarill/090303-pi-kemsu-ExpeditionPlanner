9964012931f018be221ed0c0c02bef917e5fe8c6
mrsilmar <mrsilmar@gmail.com>
2026-04-18 03:27:56 +0700

    Изменил формат вывода пасринга

--- .github/scripts/Parse-BuildLog.ps1
+++ .github/scripts/Parse-BuildLog.ps1
@@ -5,14 +5,32 @@ param(
     [string]$OutputFilePath
 )
 
+# Читаем лог сборки
 $logContent = Get-Content -Path $LogFilePath -Raw
+
+# Шаблон регулярного выражения для выделения предупреждений
 $regex = '(.*):\s*(warning\s\w+)\:\s*(.*)'
-$matches = Select-String -Pattern $regex -InputObject $logContent -AllMatches
-$warnings = @($matches.Matches | ForEach-Object {
-    @{
-        File = $_.Groups[1].Value
-        Code = $_.Groups[2].Value
-        Message = $_.Groups[3].Value
+
+# Парсим лог и получаем коллекцию предупреждений
+$warnings = Select-String -Pattern $regex -InputObject $logContent -AllMatches |
+    ForEach-Object {
+        $_ | Select-Object -Expand Matches |
+        ForEach-Object {
+            @{
+                File = ($_.Groups[1].Value -split '\\')[2..($_.Groups[1].Value.Split('\').Count - 1)] -join '\';
+                Code = $_.Groups[2].Value;
+                Message = $_.Groups[3].Value.Trim()
+            }
+        }
     }
-})
-$warnings | ConvertTo-Json | Out-File -FilePath $OutputFilePath
+
+# Сортируем по коду ошибки
+$sortedWarnings = $warnings | Sort-Object -Property Code
+
+# Генерируем текстовую таблицу
+$table = $sortedWarnings | ForEach-Object -Begin {$i=1} -Process {
+    "{0,-4}{1,-50}{2,-10}{3}" -f $i++,$_.File,$_.Code,$_.Message
+}
+
+# Сохраняем таблицу в выходной файл
+$table | Out-File -FilePath $OutputFilePath -Encoding UTF8
