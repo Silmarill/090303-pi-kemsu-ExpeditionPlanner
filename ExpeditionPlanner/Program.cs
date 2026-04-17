4fd8aa933f726ba4e2abffe7db07c518cade8de9
v01d <Kevin_Vearez@mail.ru>
2026-04-17 12:42:37 +0700

    integrate DiplomaticMission into main program
    Added DiplomaticMissionFactory to the list of factories. The program outputs 4 types of missions, including diplomatic.

--- ExpeditionPlanner/Program.cs
+++ ExpeditionPlanner/Program.cs
@@ -1,37 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
-
-// Обратите внимание на совпадение пространств имен и папок
 using ExpeditionPlanner.Models;
 using ExpeditionPlanner.Factories;
 
 namespace ExpeditionPlanner {
   internal class Program {
     static void Main() {
-      // Создаём фабрики разных типов
-      List<MissionFactory> factories = new List<MissionFactory>
+      // Объявление фабрик миссий (список)
+      List<MissionFactory> factories;
+
+      factories = new List<MissionFactory>
       {
-            new ExplorationMissionFactory(),
-            new CombatMissionFactory(),
-            new CargoMissionFactory()
-        };
+        new ExplorationMissionFactory(),
+        new CombatMissionFactory(),
+        new CargoMissionFactory(),
+        new DiplomaticMissionFactory()
+      };
 
-      Console.WriteLine("=== План миссий на месяц ===\n");
+      Console.WriteLine("=== Mission plan for the month ===\n");
 
-      foreach (var factory in factories) {
-        // Фабричный метод создаёт миссию, но мы не знаем, какого именно типа - метож у всех одинаковый
-        Mission mission = factory.CreateMission();
+      // Выполнение миссий, созданных фабриками
+      foreach (MissionFactory factory in factories) {
+        // Создание миссии через фабрику
+        Mission mission;
 
-        Console.WriteLine($"Миссия: {mission.Name} (длительность: {mission.Duration} дней)");
+        mission = factory.CreateMission();
+
+        // Выполнение миссии и вывод отчета
+        Console.WriteLine($"Mission: {mission.Name} (duration: {mission.Duration} days)");
         mission.Execute();
         Console.WriteLine(mission.GetReport());
         Console.WriteLine();
       }
-
-      // Легко добавить новую миссию, не меняя существующий код!
-      //MissionFactory newFactory = new DiplomaticMissionFactory(); // придумаем позже
-      //Mission diplomaticMission = newFactory.CreateMission();
-      //diplomaticMission.Execute();
     }
   }
 }
