775a294c0dc37eb67a29ee87e98453bf3db81bdc
lol <nexusstail@gmail.com>
2026-04-17 09:10:43 +0700

    добавлен DiplomaticMissionFactory

---
+++ ExpeditionPlanner/Factories/DiplomaticMissionFactory.cs
@@ -0,0 +1,14 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpeditionPlanner.Factories {
+      public class DiplomaticMissionFactory : MissionFactory {
+      public override Mission CreateMission() {
+        return new DiplomaticMission();
+      }
+    }
+  }
