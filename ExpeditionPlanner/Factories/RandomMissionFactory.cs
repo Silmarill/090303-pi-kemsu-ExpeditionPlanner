51bf41e3e570d0a93f606a0fa67ab6c262eafc93
Konstantin <H1dra5@yandex.ru>
2026-04-17 09:30:50 +0700

    Начал реализацию 3-го этапа

---
+++ ExpeditionPlanner/Factories/RandomMissionFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpeditionPlanner.Factories {
+  public class RandomMissionFactory : MissionFactory {
+
+    RandomMissionFactory() {
+      List<MissionFactory> _factories;
+      _factories = new List<MissionFactory>();
+      _factorie
+    }
+  }
+}
