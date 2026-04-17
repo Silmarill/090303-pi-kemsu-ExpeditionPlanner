7a71b34a215f5857727ad442d5a612c160067c00
JetRex <v.bazanov7@gmail.com>
2026-04-17 08:31:19 +0700

    Task one complite

--- ExpeditionPlanner/Models/DiplomaticMission.cs
+++ ExpeditionPlanner/Models/DiplomaticMission.cs
@@ -1,10 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ExpeditionPlanner.Models {
-  internal class DiplomaticMission {
+  public class DiplomaticMission : Mission {
+    public DiplomaticMission() {
+      Name = "Дипломатическая миссия";
+      Duration = 20;
+    }
+
+    public override void Execute() {
+      Console.WriteLine(" Переговоры с инопланетной цивилизацией, подписание договора");
+    }
+
+    public override string GetReport() {
+      return $" {Name}: заключено 3 торговых соглашения";
+    }
   }
 }
