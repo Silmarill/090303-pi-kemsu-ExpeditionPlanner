52298cc387ebeb29c5c5ed0602d38489acbc03f6
lol <nexusstail@gmail.com>
2026-04-17 09:11:04 +0700

    Добавлен DiplomaticMission

---
+++ ExpeditionPlanner/Models/DiplomaticMission.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpeditionPlanner.Models {
+  
+  public class DiplomaticMission: Mission {
+    public DiplomaticMission(): base("Дипломатическая миссия", 20){}
+
+    public override void Execute() {
+      Console.WriteLine("Переговоры с инопланетной цивилизцацией, подписание договора");
+
+    }
+    public override string GetReport() {
+      return "Дипломатическая миссия: заключено 3 торговых соглошения";
+      
+    }
+  }
+}
