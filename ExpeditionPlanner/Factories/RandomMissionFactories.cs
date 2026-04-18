6f65dd2e4fabcab23190a1ca652177444534617c
Konstantin <H1dra5@yandex.ru>
2026-04-18 16:54:52 +0700

    Сделал 3 задание

---
+++ ExpeditionPlanner/Factories/RandomMissionFactories.cs
@@ -0,0 +1,30 @@
+﻿using ExpeditionPlanner.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpeditionPlanner.Factories {
+  public class RandomMissionFactories : MissionFactory {
+    private List<MissionFactory> _factories = new List<MissionFactory>();
+    public Random _random = new Random();
+    int randomIndex;
+
+    public RandomMissionFactories() {
+      _factories.Add(new CargoMissionFactory());
+      _factories.Add(new CombatMissionFactory());
+      _factories.Add(new DiplomaticMissionFactory());
+      _factories.Add(new ExplorationMissionFactory());
+      _factories.Add(new RescueMissionFactory(0));
+    }
+
+    public override Mission CreateMission() {
+      randomIndex = _random .Next(_factories.Count);
+      MissionFactory randomFactory = _factories[randomIndex];
+      Mission newMission = randomFactory.CreateMission();
+      return newMission;
+    }
+  }
+}
+
