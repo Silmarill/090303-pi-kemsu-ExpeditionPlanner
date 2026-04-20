9ad676a7df5203eb2fd7fd479b3cdea1b11157ba
v01d <Kevin_Vearez@mail.ru>
2026-04-20 21:50:32 +0700

    Added the Factory for creating rescue missions

---
+++ ExpeditionPlanner/Factories/RescueMissionFactory.cs
@@ -0,0 +1,18 @@
+﻿using ExpeditionPlanner.Models;
+
+namespace ExpeditionPlanner.Factories {
+  // Фабрика для создания спасательных миссий
+  public class RescueMissionFactory : MissionFactory {
+    private int _peopleCount;
+
+    // Принимает количество людей, которых нужно спасти, для создания миссии
+    public RescueMissionFactory(int peopleCount) {
+      _peopleCount = peopleCount;
+    }
+
+    // Создает и возвращает новую спасательную миссию с заданным количеством людей
+    public override Mission CreateMission() {
+      return new RescueMission(_peopleCount);
+    }
+  }
+}
