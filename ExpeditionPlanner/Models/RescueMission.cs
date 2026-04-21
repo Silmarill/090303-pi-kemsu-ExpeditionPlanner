48147c470974396150f1bef0eca484631eaa0608
AssistSPB <ungefugnikita@gmail.com>
2026-04-21 23:09:21 +0700

    Mission complited

---
+++ ExpeditionPlanner/Models/RescueMission.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ExpeditionPlanner.Models {
+  public class RescueMission : Mission {
+    private readonly int _peopleCount;
+
+    public RescueMission(int peopleCount) {
+      _peopleCount = peopleCount;
+      _name = "Спасательная миссия";
+      Duration = 5 + peopleCount / 10;
+    }
+
+    public override void Execute() {
+      Console.WriteLine($" Спасательная операция: эвакуация {_peopleCount} человек");
+    }
+
+    public override string GetReport() {
+      return $" {_name}: спасено {_peopleCount} человек";
+    }
+  }
+}
--- ExpeditionPlanner/Program.cs
+++ ExpeditionPlanner/Program.cs
@@ -1,38 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
-
-// Обратите внимание на совпадение пространств имен и папок
 using ExpeditionPlanner.Factories;
 using ExpeditionPlanner.Models;
 
 namespace ExpeditionPlanner {
   public class Program {
     public static void Main() {
-      // Создаём фабрики разных типов
       List<MissionFactory> factories = new List<MissionFactory>
       {
-            new ExplorationMissionFactory(),
-            new CombatMissionFactory(),
-            new CargoMissionFactory()
+        new ExplorationMissionFactory(),
+        new CombatMissionFactory(),
+        new CargoMissionFactory(),
+        new DiplomaticMissionFactory(),
+        new RescueMissionFactory(58)
       };
 
-      Console.WriteLine("=== План миссий на месяц ===\n");
+      RandomMissionFactory randomFactory = new RandomMissionFactory();
+
+      Console.WriteLine("=== СЛУЧАЙНАЯ МИССИЯ (ЗАДАНИЕ 3) ===\n");
+      Mission randomMission = randomFactory.CreateMission();
+      Console.WriteLine($"Тип: {randomMission.GetType().Name}");
+      Console.WriteLine($"Миссия: {randomMission._name} (длительность: {randomMission.Duration} дней)");
+      randomMission.Execute();
+      Console.WriteLine(randomMission.GetReport());
+      Console.WriteLine();
+
+      Console.WriteLine("=== ВСЕ ТИПЫ МИССИЙ ===\n");
 
       foreach (MissionFactory factory in factories) {
-        // Фабричный метод создаёт миссию, но мы не знаем, какого именно типа - метож у всех одинаковый
         Mission mission = factory.CreateMission();
-
         Console.WriteLine($"Миссия: {mission._name} (длительность: {mission.Duration} дней)");
         mission.Execute();
         Console.WriteLine(mission.GetReport());
         Console.WriteLine();
       }
-
-      // Push Test Actions
-      // Легко добавить новую миссию, не меняя существующий код!
-      // MissionFactory newFactory = new DiplomaticMissionFactory(); // придумаем позже
-      // Mission diplomaticMission = newFactory.CreateMission();
-      // diplomaticMission.Execute();
     }
   }
 }
