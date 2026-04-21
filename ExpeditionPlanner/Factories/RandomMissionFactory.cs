713e51e1cc4b929ed548004853b3e2f8ac10e1ee
RedTerror <anton-nekrasov07@mail.ru>
2026-04-21 21:22:18 +0700

    OneShotMan

---
+++ ExpeditionPlanner/Factories/RandomMissionFactory.cs
@@ -0,0 +1,27 @@
+﻿using ExpeditionPlanner.Models;
+using System;
+using System.Collections.Generic;
+
+namespace ExpeditionPlanner.Factories {
+  public class RandomMissionFactory : MissionFactory {
+
+    private static Random random = new Random();
+
+    private static int _peopleCount = random.Next(1, 1000);
+
+    private List<Mission> missions = new List<Mission>
+    {
+            new CargoMission(),
+            new CombatMission(),
+            new DiplomaticMission(),
+            new DiplomaticMission(),
+            new RescueMission(_peopleCount)
+    };
+
+    public override Mission CreateMission() {
+      Mission selectedMission = missions[random.Next(missions.Count)];
+
+      return selectedMission;
+    }
+  }
+}
