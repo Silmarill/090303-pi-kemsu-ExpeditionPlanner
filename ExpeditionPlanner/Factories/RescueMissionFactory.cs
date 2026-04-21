727c60bf0d1c708554cd033c0c09ac874f2bb542
neldori <nexusstail@gmail.com>
2026-04-21 10:47:53 +0700

    изменил RescueMissionF

--- ExpeditionPlanner/Factories/RescueMissionFactory.cs
+++ ExpeditionPlanner/Factories/RescueMissionFactory.cs
@@ -1,10 +1,20 @@
-﻿using System;
+﻿using ExpeditionPlanner.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ExpeditionPlanner.Factories {
-  internal class RescueMissionFactory {
+  public class RescueMissionFactory : MissionFactory {
+    int _peopleCount;
+
+    public RescueMissionFactory(int peopleCount) {
+      _peopleCount = peopleCount;
+    }
+
+    public override Mission CreateMission() {
+      return new RescueMission(_peopleCount);
+    }
   }
 }
