de1e725800cd67e4712181e4da977bd0c0241e52
m1tsuyaaa <stepan.katkovxx@gmail.com>
2026-04-23 05:25:36 +0700

    added code

---
+++ ExpeditionPlanner/Factories/RandomMissionFactory.cs
@@ -0,0 +1,26 @@
+﻿using ExpeditionPlanner.Models;
+using System;
+using System.Collections.Generic;
+
+namespace ExpeditionPlanner.Factories {
+  public class RandomMissionFactory : MissionFactory {
+    private readonly List<MissionFactory> _factories;
+    private readonly Random _random = new Random();
+
+    public RandomMissionFactory() {
+      _factories = new List<MissionFactory>
+      {
+                new ExplorationMissionFactory(),
+                new CombatMissionFactory(),
+                new CargoMissionFactory(),
+                new DiplomaticMissionFactory(),
+                new RescueMissionFactory(50)
+            };
+    }
+
+    public override Mission CreateMission() {
+      int index = _random.Next(_factories.Count);
+      return _factories[index].CreateMission();
+    }
+  }
+}
