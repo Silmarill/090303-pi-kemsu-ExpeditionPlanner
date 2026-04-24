f6f6988c331a75002dd737123c969486ed7752f6
Saibuu Vilena <vsaybuu@bk.ru>
2026-04-24 12:33:21 +0700

    пустая строка между полями

--- ExpeditionPlanner/Models/Mission.cs
+++ ExpeditionPlanner/Models/Mission.cs
@@ -1,8 +1,11 @@
 ﻿namespace ExpeditionPlanner.Models {
   public abstract class Mission {
     public string Name;
+
     public int Duration; // в днях
+
     public abstract void Execute();
+
     public abstract string GetReport();
   }
 }
