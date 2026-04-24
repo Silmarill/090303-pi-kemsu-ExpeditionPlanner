b13e93b0c9944269e1f4e7d5b684361895517c99
dezue <mistfoll@mail.ru>
2026-04-24 14:02:36 +0700

    linter 3

--- ExpeditionPlanner/Models/RescueMission.cs
+++ ExpeditionPlanner/Models/RescueMission.cs
@@ -2,14 +2,14 @@
 
 namespace ExpeditionPlanner.Models {
   public class RescueMission : Mission {
-    public int baseDurationDays = 5;
+    public int BaseDurationDays = 5;
     public int peoplePerExtraDay = 10;
     private readonly int _peopleCount;
 
     public RescueMission(int peopleCount) {
       _peopleCount = peopleCount;
       Name = "Спасательная миссия";
-      Duration = baseDurationDays + (peopleCount / peoplePerExtraDay);
+      Duration = BaseDurationDays + (peopleCount / peoplePerExtraDay);
     }
 
     public override void Execute() {
