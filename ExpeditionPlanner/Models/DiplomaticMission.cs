bba9b367760d37f53ca050aca9f86d63d1b4f2b8
Saibuu Vilena <vsaybuu@bk.ru>
2026-04-24 12:57:52 +0700

    пустая строка после константы добавлена

--- ExpeditionPlanner/Models/DiplomaticMission.cs
+++ ExpeditionPlanner/Models/DiplomaticMission.cs
@@ -3,17 +3,18 @@
 namespace ExpeditionPlanner.Models {
   public class DiplomaticMission : Mission {
     private const int DefaultDiplomaticDuration = 20;
+
     public DiplomaticMission() {
       Name = "Дипломатическая миссия";
       Duration = DefaultDiplomaticDuration;
     }
 
     public override void Execute() {
-      Console.WriteLine(" 🤝 Переговоры с инопланетной цивилизацией, подписание договора ");
+      Console.WriteLine("🤝 Переговоры с инопланетной цивилизацией, подписание договора");
     }
 
     public override string GetReport() {
-      return " Торговые соглашения заключены успешно ";
+      return "Торговые соглашения заключены успешно";
     }
   }
 }
