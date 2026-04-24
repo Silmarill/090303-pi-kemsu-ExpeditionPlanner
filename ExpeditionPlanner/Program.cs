e7e35f02184aca6fb7457a2e22fc42657be4192f
Saibuu Vilena <vsaybuu@bk.ru>
2026-04-24 13:05:20 +0700

    Порядок using по алфавиту

--- ExpeditionPlanner/Program.cs
+++ ExpeditionPlanner/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
-// Обратите внимание на совпадение пространств имен и папок
-using ExpeditionPlanner.Models;
 using ExpeditionPlanner.Factories;
+using ExpeditionPlanner.Models;
 
 namespace ExpeditionPlanner {
   internal class Program {
@@ -9,9 +8,8 @@ namespace ExpeditionPlanner {
       // Создаём фабрики разных типов -> одну случайную фабрику вместо списка
       MissionFactory randomFactory = new RandomMissionFactory();
 
-      Console.WriteLine("    Случайные миссии    \n");
+      Console.WriteLine("=== Случайные миссии ===\n");
 
-        // Фабричный метод создаёт миссию, но мы не знаем, какого именно типа - метод у всех одинаковый
       Mission mission = randomFactory.CreateMission();
 
       Console.WriteLine($"Миссия: {mission.Name} (длительность: {mission.Duration} дней)");
@@ -19,9 +17,5 @@ namespace ExpeditionPlanner {
       Console.WriteLine(mission.GetReport());
       Console.WriteLine();
     }
-    // Легко добавить новую миссию, не меняя существующий код!
-    // MissionFactory newFactory = new DiplomaticMissionFactory(); // придумаем позже
-    // Mission diplomaticMission = newFactory.CreateMission();
-    // diplomaticMission.Execute();
   }
 }
