281389811c840878894a91cf31195c0f5797c304
ksmushkov-cmd <ksmushkov@bk.ru>
2026-04-24 13:29:55 +0700

    поправил

--- ExpeditionPlanner/Models/DiplomaticMission.cs
+++ ExpeditionPlanner/Models/DiplomaticMission.cs
@@ -3,21 +3,17 @@
 namespace ExpeditionPlanner.Models {
   public class DiplomaticMission : Mission {
 
-    private string _reportMessage;
-    private const int DefaultDuration = 20;
-
     public DiplomaticMission() {
       Name = "Дипломатическая миссия";
-      Duration = DefaultDuration;
-      _reportMessage = $"{Name}: заключено 3 торговых соглашения";
+      Duration = 20;
     }
 
     public override void Execute() {
-      _reportMessage = "Переговоры с инопланетной цивилизацией";
+      Console.WriteLine(" Переговоры с инопланетной цивилизацией");
     }
 
     public override string GetReport() {
-      return _reportMessage;
+      return $"{Name}: заключено 3 торговых соглашения";
     }
 
   }
