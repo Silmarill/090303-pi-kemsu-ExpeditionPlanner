eb2b9f22607e1fba2f773733ec67a0cad80727c7
Vova200804 <sanek_sanek_24@mail.ru>
2026-04-26 23:31:54 +0700

    чет не то происходит

--- ExpeditionPlanner/Models/DiplomaticMission.cs
+++ ExpeditionPlanner/Models/DiplomaticMission.cs
@@ -1,14 +1,18 @@
 ﻿namespace ExpeditionPlanner.Models {
   public class DiplomaticMission : Mission {
     private int _tradeAgreementsCount;
+
     public DiplomaticMission() {
       Name = "Дипломатическая миссия";
       Duration = 2;
       _tradeAgreementsCount = 0;
+
     }
+
     public override void Execute() {
       _tradeAgreementsCount = 3;
     }
+
     public override string GetReport() {
       return $"{Name}: заключено {_tradeAgreementsCount} торговых соглашения";
     }
