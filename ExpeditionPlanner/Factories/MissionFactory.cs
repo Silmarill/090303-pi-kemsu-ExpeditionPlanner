a11b6ac1202071859d6ea16c4d1fe8a307ef7960
wacpercs <wacpercs@gmail.com>
2026-04-28 21:08:30 +0700

    fixed "IDE0055 Fix formatting"

--- ExpeditionPlanner/Factories/MissionFactory.cs
+++ ExpeditionPlanner/Factories/MissionFactory.cs
@@ -2,7 +2,6 @@
 
 namespace ExpeditionPlanner.Factories {
   public abstract class MissionFactory {
-    
     // Тот самый ФАБРИЧНЫЙ МЕТОД — возвращает абстрактный продукт
     public abstract Mission CreateMission();
   }
