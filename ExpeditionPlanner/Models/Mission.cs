0c504442c62806948c8166a19dfde852cf6c4231
Jameson54 <hehlol123123@mail.ru>
2026-04-29 12:56:56 +0700

    The bug fixes have been completed!

--- ExpeditionPlanner/Models/Mission.cs
+++ ExpeditionPlanner/Models/Mission.cs
@@ -1,8 +1,11 @@
 ﻿namespace ExpeditionPlanner.Models {
   public abstract class Mission {
     public string Name;
+
     public int Duration;
+
     public abstract void Execute();
+
     public abstract string GetReport();
   }
 }
